8567b8a7251d8b8b99b8bcb52654cc1bae3b5f74
Vitor Reis <vitoramr07@gmail.com>
2021-04-04 22:21:45 -0300

    commit inicial

---
+++ README.md
@@ -0,0 +1,6 @@
+# Liga da Justiça Matos cut :bat:
+
+
+
+OLÁ! BEM VINDO AO LIGA DA JUSTIÇA *MATOS CUT*.
+
---
+++ funcoes.js
@@ -0,0 +1,12 @@
+
+
+
+function Esconder(){
+    
+    $('#im1').fadeIn(10000);
+    $('#im2').fadeIn(13000);
+    $('#im3').fadeIn(16000);
+    $('#im4').fadeIn(20000);
+    
+    
+}
