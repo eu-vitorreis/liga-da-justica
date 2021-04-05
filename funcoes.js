857e4bffaa07ba7ddf4f47236913614013d04582
Vitor Reis <vitoramr07@gmail.com>
2021-04-04 22:42:01 -0300

    Atualização geral

--- funcoes.js
+++ funcoes.js
@@ -1,12 +1,7 @@
-
-
-
-function Esconder(){
-    
+//função aparecr com o fadein que limita a velocidade que as imagens vão aparecer
+function Aparecer(){
     $('#im1').fadeIn(10000);
     $('#im2').fadeIn(13000);
     $('#im3').fadeIn(16000);
     $('#im4').fadeIn(20000);
-    
-    
 }
